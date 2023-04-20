06ea437de411889d1af228afe1c52023e077115e
Rihard4a <118355184+Rihard4a@users.noreply.github.com>
2023-04-20 08:16:22 +0300

    Created repo

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetFlights(int id)
+        {
+            return Ok();
+        }
+    }
+}
